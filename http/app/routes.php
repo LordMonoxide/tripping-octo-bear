bcd7515c6c63680b14f3a625f38e92ac28fe054f
Corey Frenette <corey@narwhunderful.com>
2014-01-26 13:58:49 -0400

    Update routes

--- http/app/routes.php
+++ http/app/routes.php
@@ -6,13 +6,15 @@ Route::group(['prefix' => 'api'], function() {
     Route::put ('/register', ['as' => 'api.auth.register', 'uses' => 'api\AuthController@register']);
     Route::post('/login',    ['as' => 'api.auth.login',    'uses' => 'api\AuthController@login']);
     Route::post('/logout',   ['as' => 'api.auth.logout',   'uses' => 'api\AuthController@logout']);
+    Route::get ('/security', ['as' => 'api.auth.security', 'uses' => 'api\AuthController@security']);
+    Route::post('/security', ['as' => 'api.auth.security', 'uses' => 'api\AuthController@unlock']);
   });
   
   Route::group(['prefix' => 'storage'], function() {
     Route::group(['prefix' => 'characters'], function() {
-      Route::get   ('/',       ['as' => 'api.storage.characters',        'uses' => 'api\storage\CharacterController@all']);
-      Route::put   ('/create', ['as' => 'api.storage.characters.create', 'uses' => 'api\storage\CharacterController@create']);
-      Route::delete('/delete', ['as' => 'api.storage.characters.delete', 'uses' => 'api\storage\CharacterController@delete']);
+      Route::get   ('/', ['as' => 'api.storage.characters', 'uses' => 'api\storage\CharacterController@all']);
+      Route::put   ('/', ['as' => 'api.storage.characters', 'uses' => 'api\storage\CharacterController@create']);
+      Route::delete('/', ['as' => 'api.storage.characters', 'uses' => 'api\storage\CharacterController@delete']);
     });
   });
 });
